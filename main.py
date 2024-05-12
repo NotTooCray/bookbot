ecea33836b860e09f094530daf5e7031404d4d85
NotTooCray <nikita.dubovik@pm.me>
2024-05-12 16:42:23 +0200

    reworked the countLetters function to return the value instead of printing it, sort_on function added and dictionary was redone as well

--- main.py
+++ main.py
@@ -11,18 +11,39 @@ def main():
         to_lower = content.lower()
         to_lower_allowed = "abcdefghijklmnopqrstuvwxyz"
         letter_count = {}
+        
         for letter in to_lower:
             if letter in to_lower_allowed:
                 if letter in letter_count:
                     letter_count[letter] += 1
                 else:
                     letter_count[letter] = 1
-        
-        return print(f"The count of the letters is as follows: {letter_count}")
+        return letter_count #print(f"The count of the letters is as follows: {letter_count}")
 
-    return print(content), countWords(content), countLetters(content)
+    def sort_on(d):
+        return d["letter"]
 
+    # function calls
+    letter_counts = countLetters(content)
+    letter_list = [{"letter": key, "num": value} for key, value in letter_counts.items()]
+
+    letter_list.sort(key=sort_on)
     
-    
+    result_of_count = "Sorted Letter Counts:\n"
+    for item in letter_list:
+        result_of_count += f"  {item['letter']}: {item['num']}\n"
+
+
+    return (print(content),
+            print("\n"),
+            print("--- Begin report of books/frankenstein.txt ---"), 
+            print("\n"),
+            countWords(content), 
+            print("\n"),
+            print(result_of_count), 
+            print("--- End report ---"))
+
 main()
+
+
 
